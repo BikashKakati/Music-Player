7b685b75614d4662a0226ae61982bcb7e07be3ef
Bikash Kakati <bikashkakati796@gmail.com>
2024-09-22 20:58:45 +0530

    Add: Made utility function to Create audio

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,33 @@
-export function getLimitedFormattedText(text:string="",requiredLength:number):string{
-    return text.length > requiredLength ? `${text.slice(0,requiredLength+1)}...`:text;
+import { Audio } from "expo-av";
+
+export function getLimitedFormattedText(
+  text: string = "",
+  requiredLength: number
+): string {
+  return text.length > requiredLength
+    ? `${text.slice(0, requiredLength + 1)}...`
+    : text;
 }
 
-export function getFormattedImageUrl(imageUrl:string){
-    return imageUrl?.replace("{w}", "300")?.replace("{h}", "300") || "";
+export function getFormattedImageUrl(imageUrl: string) {
+  return imageUrl?.replace("{w}", "300")?.replace("{h}", "300") || "";
+}
+
+export async function handleAudio(playbackUrl: string) {
+  try {
+    await Audio.setAudioModeAsync({
+      playsInSilentModeIOS: true,
+      staysActiveInBackground: true,
+      shouldDuckAndroid: false,
+    });
+    const soundState = await Audio.Sound.createAsync(
+      { uri: playbackUrl },
+      { shouldPlay: true, isLooping: false }
+    );
+
+    return soundState;
+  } catch (error) {
+    console.error("Error initializing audio:", error);
+    throw error; // Rethrow to handle it properly
+  }
 }
