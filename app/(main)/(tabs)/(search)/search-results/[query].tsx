c6ba2fdf472eb95768dd8e37da904bd1c1fe8477
Bikash Kakati <bikashkakati796@gmail.com>
2024-09-29 19:41:29 +0530

    Add: Dynamic route for genre wise songs

--- app/(main)/(tabs)/(search)/search-results/[query].tsx
+++ app/(main)/(tabs)/(search)/search-results/[query].tsx
@@ -1,16 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
-import { useGetSongsByGenreQuery } from '@/services/redux/apiReducers/songApi';
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { Stack, router, useLocalSearchParams } from "expo-router";
+import { useGetSongsByGenreQuery } from "@/services/redux/apiReducers/songApi";
+import Wrapper from "@/components/Wrapper";
+import Loader from "@/components/Loader";
+import SongList from "@/components/songList/SongList";
+import { setSongsQueueList } from "@/services/redux/sliceReducers/songSlice";
+import { useDispatch } from "react-redux";
 
 const SearchResults = () => {
-    const {query} = useLocalSearchParams();
-    const {data, isLoading} = useGetSongsByGenreQuery({genreType:query,limit:10});
-  return (
-    <View>
-      <Text className='text-white'>SearchResults:{query}</Text>
-    </View>
-  )
-}
+  const { query } = useLocalSearchParams();
+  const dispatch = useDispatch();
+  const { data: response, isLoading } = useGetSongsByGenreQuery({
+    genreType: query,
+    limit: 10,
+  });
 
-export default SearchResults
+  useEffect(() => {
+    if (response) {
+      dispatch(setSongsQueueList(response?.data || []));
+    }
+  }, [response]);
+
+  return (
+    <Wrapper safeArea={false}>
+      <Stack.Screen
+        options={{
+          title: `Dive into ${(query as string).toLowerCase()} music`,
+        }}
+      />
+      {isLoading ? <Loader /> : <SongList songList={response?.data || []} />}
+    </Wrapper>
+  );
+};
+
+export default SearchResults;
