6e2a9f8bd5ad5377e999178f594ecd963b3c67ae
Bikash Kakati <bikashkakati796@gmail.com>
2024-10-01 01:08:50 +0530

    Add: Search api

--- app/(main)/(tabs)/(search)/index.tsx
+++ app/(main)/(tabs)/(search)/index.tsx
@@ -1,13 +1,26 @@
 import GenresCard from "@/components/GenresCard";
 import Wrapper from "@/components/Wrapper";
 import { genres } from "@/constants";
+import { useLazyGetSongsBySearchQueryQuery } from "@/services/redux/apiReducers/songApi";
 import { Search } from "lucide-react-native";
 import React, { useState } from "react";
 import { Pressable, ScrollView, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
 const SearchTrack = () => {
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [getSongsBySearchQuery] = useLazyGetSongsBySearchQueryQuery();
+
+  async function handleSearchQuery() {
+    const formattedSearchQuery = searchQuery.toLocaleLowerCase().split(" ").join("%20");
+    console.log(formattedSearchQuery);
+    const { data: response } = await getSongsBySearchQuery({
+      searchQuery: formattedSearchQuery,
+      limit: 10,
+    });
+    console.log(response.data.tracks);
+  }
+
   return (
     <Wrapper>
       <View className="border border-zinc-600 py-2 pl-5 pr-2 rounded-full flex flex-row items-center">
@@ -17,8 +30,9 @@ const SearchTrack = () => {
           onChangeText={setSearchQuery}
           placeholderTextColor={"#999"}
           placeholder="Search here..."
+          cursorColor={"#fff"}
         />
-        <Pressable className="w-8">
+        <Pressable className="w-8" onPress={handleSearchQuery}>
           <Search size={19} className="text-muted mx-auto" />
         </Pressable>
       </View>
